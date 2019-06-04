109ce517d2d194f179aa6a7009283a2ffeb50139
GriKate <kate23april@yandex.ru>
2019-06-04 16:50:08 +0300

    исправила ошибки

--- 01.js
+++ 01.js
@@ -2,20 +2,27 @@ const products = [
     {id: 1, title: 'Notebook', price: 2000,},
     {id: 2, title: 'Mouse', price: 30,},
     {id: 3, title: 'Keyboard', price: 55,},
-    {id: 4, title: 'Gamepad', price: 75,},
+    {id: 4, title: 'Gamepad',},
 ];
 
-const renderProduct = (title = products.title, price = products.price) => {
+const renderProduct = (title, price = 'Out of stock', img = 'https://placehold.it/200x150') => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
+                <img src="${img}">
+                <div class="item-desc">
+                    <h3>${title}</h3>
+                    <p>${price}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
             </div>`;
 };
 
 const renderPage = list => {
-    const productList = list.map(item => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productList.join('\r\n');
+    // const productList = list.map(item => renderProduct(item.title, item.price));
+    // document.querySelector('.products').innerHTML = productList.join('\r\n');
+    for (let product of list) {
+        document.querySelector('.products').insertAdjacentHTML('beforeend', renderProduct(product.title, product.price))
+    }
 };
 
 renderPage(products);
+
